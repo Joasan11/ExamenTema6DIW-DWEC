d6ef77204f90f0533d6ef0276a8598da0deda6d2
Joaquin Sanchez <joaquinsanchezmiravalles@alumnos.tuniversformacion.es>
2024-02-21 09:17:03 +0100

    Ahora añadimos las funciones que realizaremos en js

---
+++ build/js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function(){
+    iniciarApp()
+    scrollNow()
+});
+
+function iniciarApp(){
+    
+}
+
+function crearGaleria(){
+
+}
+
+function mostrarImagen(){
+
+}
+
+function imagenExiste(){
+
+}
+
+function scrollNow(){
+
+}
