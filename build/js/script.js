85eb8bc69dd851a4975a640a703e6c14e93f253a
Joaquin Sanchez <joaquinsanchezmiravalles@alumnos.tuniversformacion.es>
2024-02-21 09:39:07 +0100

    Acabamos el crearGaleria

--- build/js/script.js
+++ build/js/script.js
@@ -12,8 +12,33 @@ function iniciarApp(){
     yearSpan.textContent = currentYear;
 }
 
-function crearGaleria(){
+function crearGaleria() {
+    const galeria = document.querySelector(".galeria-imagenes");
 
+    for (let i = 1; i <= 12; i++) {
+        const imagen = document.createElement("img");
+        const imageName = `${i}.jpg`;
+
+        const extensiones = ['webp', 'jpeg', 'jpg'];
+        for (const ext of extensiones) {
+            const src = `build/img/pequeña/${i}.${ext}`;
+            if (imagenExiste(src)) {
+                imagen.src = src;
+                break;
+            }
+        }
+
+        imagen.alt = "imagen galeria";
+        imagen.loading = "lazy";
+
+        (function (indice) {
+            imagen.onclick = function () {
+                mostrarImagen(indice);
+            };
+        })(i);
+
+        galeria.appendChild(imagen);
+    }
 }
 
 function mostrarImagen(){
@@ -27,5 +52,6 @@ function imagenExiste(){
 function scrollNow(){
 
 }
+
 
 
